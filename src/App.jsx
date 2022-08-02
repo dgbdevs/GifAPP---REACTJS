540b1ad2518ecf5cdbfce0a280655a8bc2f8f8f8
dgbdevs <dgb.develop@gmail.com>
2022-08-02 02:57:43 -0300

    aun falta resolver la eliminacion

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,38 @@ import { useState, useEffect } from 'react';
 function App() {
   const [search, setSearch] = useState('')
   const [data, setData] = useState()
-  const [tipo, setTipo] = useState('stickers')
+  const [tipo, setTipo] = useState('gifs')
+  const [selected,setSelected]=useState([])
 
-  const buscarGif = async () => {
-    fetch(`https://api.giphy.com/v1/${tipo}/search?api_key=${import.meta.env.VITE_API_GIPHY}&q=${search}&limit=10&offset=0&rating=g&lang=es`)
+  //Funcion para buscar gif en GiPHY
+  const buscarGif = () => {
+    fetch(`https://api.giphy.com/v1/${tipo}/search?api_key=${import.meta.env.VITE_API_GIPHY}&q=${search}&limit=15&offset=0&rating=g&lang=es`)
       .then(response => response.json())
       .then(data => setData(data))
   }
 
+  //Cambio de parametro de busqueda Switch
   const cambiarTipo = () => {
     if (document.getElementsByName('tipo')[0].checked) {
-      setTipo('gifs')
-    } else {
       setTipo('stickers')
+    } else {
+      setTipo('gifs')
     }
   }
 
+  //ejecuto la funcion cuando cambia search o tipo
   useEffect(() => {
     buscarGif();
   }, [search, tipo])
 
-
+  
+  //Borro el gif cargado
+  const delSelected = (i) =>{
+    let arr = selected
+    arr.splice(i,1)
+    setSelected(arr)
+   
+  }
 
   return (
     <div className="App">
@@ -43,15 +54,45 @@ function App() {
       </div>
 
       <h2>{search}</h2>
+      <hr></hr>
+      <div className='gif-container'>
       {data ?
         data.data.map((d, index) => {
           return (
-            <img src={d.images.fixed_height_small.url} key={index}></img>
+            /* onClick cargo gifs a selected */
+            <div key={index}   className='resultImg' onClick={()=>setSelected([...selected,d.images.fixed_height_small.url])}>
+              <img               
+                src={d.images.fixed_height_small.url}
+                               
+              ></img>
+            </div>
           )
         })
         :
         <></>
       }
+      </div> 
+      <hr></hr>
+      <div >
+        {selected?
+        <div className='gif-container'>
+          {/* MUESTRO GIF SELECCIONADOS */}
+          {selected.map((s,index)=>{
+             return (
+              <div  className='resultImg' onClick={()=>delSelected(index)}>
+                <img               
+                  src={s}
+                  key={index}                
+                ></img>
+              </div>
+            )
+
+          })}
+        </div>
+        :
+        <></>  
+      }  
+      </div>     
     </div>
   )
 }
